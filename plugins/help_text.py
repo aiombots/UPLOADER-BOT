fdc056059f02e8f491e1837685f53f2318f45a1e
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-05 19:36:21 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -19,7 +19,7 @@ from database.adduser import AddUser
 from pyrogram import Client as Clinton
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 
 @Clinton.on_message(filters.private & filters.command(["help"]))
@@ -43,15 +43,24 @@ async def start(bot, update):
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
-                    ),
-                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
-                ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham")],
-            ]
-        ),
-        reply_to_message_id=update.message_id
-    )
+                [[
+                  InlineKeyboardButton("", ),
+                  InlineKeyboardButton("", )
+                ],[
+                  InlineKeyboardButton("", )
+                ]]
+
+@Clinton.on_callback_query()
+async def callback(bot: Clinton, update: CallbackQuery):
+    if update.data == "start":
+        await update.message.edit(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=InlineKeyboardMarkup(
+                [[
+                  InlineKeyboardButton("", ),
+                  InlineKeyboardButton("", )
+                ],[
+                  InlineKeyboardButton("", )
+                ]]
+        )
+        )    
