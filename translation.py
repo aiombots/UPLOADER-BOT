7709335e7530d3ed6b884be0e8efe43bbd84261f
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-06 20:38:20 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,20 +5,24 @@ Tʜɪꜱ Iꜱ A Pᴏᴡᴇʀꜰᴜʟ Uʀʟ Uᴘʟᴏᴀᴅᴇʀ Bᴏᴛ.
 
 Yᴏᴜ Cᴀɴ Uᴘʟᴏᴀᴅ Fɪʟᴇs/Vɪᴅᴇᴏ Fʀᴏᴍ Dɪʀᴇᴄᴛ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ
 
-Cʟɪᴄᴋ Oɴ Hᴇʟᴩ Bᴜᴛᴛᴏɴ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ...
+Cʟɪᴄᴋ Oɴ /help Cᴏᴍᴍᴀɴᴅ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ...
 
-Pᴏᴡᴇʀᴇᴅ ʙʏ : @AIOM_BOTS"""
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+Pᴏᴡᴇʀᴇᴅ Bʏ : @AIOM_BOTS"""
+    FORMAT_SELECTION = "Sᴇʟᴇᴄᴛ Tʜᴇ Dᴇsɪʀᴇᴅ Fᴏʀᴍᴀᴛ"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "📥Downloading..."
-    UPLOAD_START = "📤Uploading..."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @Uploader_X_bot\n\n<b>Join : @Space_X_Bots</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@Uploader_X_Bot"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    CUSTOM_CAPTION_UL_FILE = "{}"
+    DOWNLOAD_START = "Dᴏᴡɴʟᴏᴀᴅɪɴɢ..."
+    UPLOAD_START = "Uᴘʟᴏᴀᴅɪɴɢ..."
+    RCHD_TG_API_LIMIT = """Dᴏᴡɴʟᴏᴀᴅᴇᴅ Iɴ {} Sᴇᴄᴏɴᴅs
+
+Dᴇᴛᴇᴄᴛᴇᴅ Fɪʟᴇ Sɪᴢᴇ: {}
+
+Sᴏʀʀʏ. Bᴜᴛ, ɪ Cᴀɴɴᴏᴛ Uᴘʟᴏᴀᴅ Fɪʟᴇs Gʀᴇᴀᴛᴇʀ Tʜᴀɴ 2ɢʙ Dᴜᴇ Tᴏ Tᴇʟᴇɢʀᴀᴍ Aᴘɪ LIᴍɪᴛᴀᴛɪᴏɴs."""
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Tʜᴀɴᴋs Fᴏʀ Usɪɴɢ Mᴇ"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Dᴏᴡɴʟᴏᴀᴅᴇᴅ Iɴ {} Sᴇᴄᴏɴᴅs.\n\nUᴘʟᴏᴀᴅᴇᴅ Iɴ {} Sᴇᴄᴏɴᴅs.\n\nPᴏᴡᴇʀᴅ By : @AIOM_BOTS"
+    SAVED_CUSTOM_THUMB_NAIL = "Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ Sᴜᴄᴄᴇssꜰᴜʟʟʏ"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Cʟᴇᴀʀᴇᴅ Sᴜᴄᴄᴇsꜰᴜʟʟʏ."
+    CUSTOM_CAPTION_UL_FILE = "Uᴘʟᴏᴀᴅᴇᴅ Bʏ : @AIOM_BOTS"
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
     HELP_USER = """╭──────[ Hᴇʟᴩ  ]───────⍟
 │
@@ -54,7 +58,7 @@ URL | filename | username | password"""
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
 """
-    CANCEL_STR = "Process Cancelled"
+    CANCEL_STR = "Pʀᴏᴄᴇss Cᴀɴᴄᴇʟʟᴇᴅ"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     ABOUT_TEXT = """
