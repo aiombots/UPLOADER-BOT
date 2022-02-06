81e66e7a66ce45739dd629aff64c17de8490494f
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-06 17:29:10 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -21,3 +21,17 @@ async def callback(bot, msg):
         await msg.message.edit(
             text=Translation.ABOUT_TEXT
         )
+
+@Clinton.on_callback_query(filters.regex('^X0$'))
+async def delt(bot, update):
+          await update.message.delete(True)
+
+
+@Clinton.on_callback_query()
+async def button(bot, update):
+
+    cb_data = update.data
+    if "|" in cb_data:
+        await youtube_dl_call_back(bot, update)
+    elif "=" in cb_data:
+        await ddl_call_back(bot, update)
