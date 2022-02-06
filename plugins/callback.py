9a8b5b2f0e45b4adbacf8b57f0320e3d35d0a33a
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-06 17:42:50 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -7,6 +7,7 @@ from pyrogram import Client as Clinton
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
 from translation import Translation
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Clinton.on_callback_query()
 async def callback(bot, msg):
