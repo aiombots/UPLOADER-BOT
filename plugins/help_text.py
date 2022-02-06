672c56dd001f65a7bead740c6460f71055a59546
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-06 10:41:34 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -42,6 +42,16 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.mention)
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Help", callback_data="help"),
+                    InlineKeyboardButton("About", callback_data="about"),
+                ],
+                [   InlineKeyboardButton("Close", callback_data="close")],
+            ]
+        ),
+        reply_to_message_id=update.message_id
    )
 
 @Clinton.on_callback_query()
