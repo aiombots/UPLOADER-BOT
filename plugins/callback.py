2703a430178fe9a8421993e4a14a0d10e63172b6
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-06 17:26:55 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -7,16 +7,17 @@ from pyrogram import Client as Clinton
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
 
-@Clinton.on_callback_query(filters.regex('^X0$'))
-async def delt(bot, update):
-          await update.message.delete(True)
-
-
 @Clinton.on_callback_query()
-async def button(bot, update):
-
-    cb_data = update.data
-    if "|" in cb_data:
-        await youtube_dl_call_back(bot, update)
-    elif "=" in cb_data:
-        await ddl_call_back(bot, update)
+async def callback(bot, msg):
+    if msg.data == "start":
+        await msg.message.edit(
+            text=Translation.START_TEXT.format(update.from_user.mention)
+        )
+    elif msg.data == "help":
+        await msg.message.edit(
+            text="working"
+        )
+    elif msg.data == "about":
+        await msg.message.edit(
+            text=Translation.ABOUT_TEXT
+        )
