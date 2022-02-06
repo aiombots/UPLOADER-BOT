ef100bf79b61ef77ddbdf79e0322e10487b87aa7
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-06 20:08:42 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -10,61 +10,6 @@ from translation import Translation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Clinton.on_callback_query()
-async def callback(bot, msg):
-    if msg.data == "start":
-        await msg.message.edit(
-            text=f"""Hᴇʟʟᴏ 👋 , ♡
-
-Tʜɪꜱ Iꜱ A Pᴏᴡᴇʀꜰᴜʟ Uʀʟ Uᴘʟᴏᴀᴅᴇʀ Bᴏᴛ.
-
-Yᴏᴜ Cᴀɴ Uᴘʟᴏᴀᴅ Fɪʟᴇs/Vɪᴅᴇᴏ Fʀᴏᴍ Dɪʀᴇᴄᴛ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ
-
-Cʟɪᴄᴋ Oɴ Hᴇʟᴩ Bᴜᴛᴛᴏɴ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ...
-
-Pᴏᴡᴇʀᴇᴅ ʙʏ : @AIOM_BOTS""",
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Hᴇʟᴘ", callback_data="help"),
-                    InlineKeyboardButton("Aʙᴏᴜᴛ", callback_data="about"),
-                ],
-                [   InlineKeyboardButton("Cʟᴏsᴇ", callback_data="close")],
-            ]
-        )
-        )
-    elif msg.data == "help":
-        await msg.message.edit(
-            text=Translation.HELP_USER,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Hᴏᴍᴇ", callback_data="start"),
-                    InlineKeyboardButton("Aʙᴏᴜᴛ", callback_data="about"),
-                ],
-                [   InlineKeyboardButton("Cʟᴏsᴇ", callback_data="close")],
-            ]
-        )
-        )
-    elif msg.data == "about":
-        await msg.message.edit(
-            text=Translation.ABOUT_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Hᴇʟᴘ", callback_data="help"),
-                    InlineKeyboardButton("Hᴏᴍᴇ", callback_data="start"),
-                ],
-                [   InlineKeyboardButton("Cʟᴏsᴇ", callback_data="close")],
-            ]
-        )
-        )
-
-    elif msg.data == "close":
-        await msg.message.delete(True)
-
 @Clinton.on_callback_query(filters.regex('^X0$'))
 async def delt(bot, update):
           await update.message.delete(True)
@@ -78,3 +23,8 @@ async def button(bot, update):
         await youtube_dl_call_back(bot, update)
     elif "=" in cb_data:
         await ddl_call_back(bot, update)
+
+@Clinton.on_callback_query(filters.regex('^start$'))
+async def callback(bot, update):
+
+
