7e2c7a7a7c95051b45889f5b0f54a9d8077c2415
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-07 18:16:44 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -41,7 +41,7 @@ async def progress_for_pyrogram(
             ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = ╭───[ Pʀᴏɢʀᴇꜱꜱ Bᴀʀ ]───⍟
+        tmp = "╭───[ Pʀᴏɢʀᴇꜱꜱ Bᴀʀ ]───⍟
 │
 ├📁 Dᴏɴᴇ : {0} | {1}
 │
@@ -50,7 +50,7 @@ async def progress_for_pyrogram(
 ├⚡Sᴘᴇᴇᴅ : {2}/s
 │
 ├⏱️ Eᴛᴀ : {3}
-╰─[{0}{1}]─────⍟.format(
+╰─[{0}{1}]─────⍟".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
