1f39cc9c135451c2deeba3233e40bcbc0dc4efaf
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-07 18:26:51 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -237,7 +237,7 @@ async def echo(bot, update):
         await imog.delete(True)
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
+            text=Translation.FORMAT_SELECTION,
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
